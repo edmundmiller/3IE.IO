30a5e79ce6aaa7f94d25bcaf53a660912f6bae56
Daniel McSheehy <dsm140130@utdallas.edu>
2018-10-15 14:46:37 -0500

    adding routes

--- app/client/src/components/App.js
+++ app/client/src/components/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from '../pages/Landing';
+import Profile from '../pages/Profile';
 import '../css/App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <Router>
+        <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/profile" component={Profile} />
+        </Switch>
+      </Router>
       </div>
     );
   }

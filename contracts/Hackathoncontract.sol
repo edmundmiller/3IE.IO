f4a497896b2da710ea6544a2bdfce724f235a614
cylon56 <michaelnlewellen@gmail.com>
2018-10-15 17:19:04 -0500

    added dev mode

--- contracts/Hackathoncontract.sol
+++ contracts/Hackathoncontract.sol
@@ -4,6 +4,7 @@ contract Hackathoncontract {
 
 // store balances of bounty suppliers/providers
     address public owner; //The 3IE.IO account
+    bool public DEV_MODE = true; //Allows bounties to be claimed without rep requirement
 
     mapping (bytes32 => uint) public bounty_map;
     mapping (bytes32 => uint) public payout_map;
@@ -28,6 +29,8 @@ contract Hackathoncontract {
         return (x - y);
     }
 
+
+
     function create_bounty(bytes32 _hash, uint _payout, uint min_rep) public payable returns (bool success) {
         bounty_map[_hash] = safe_add(bounty_map[_hash], msg.value);
         payout_map[_hash] = safe_add(0, _payout);
@@ -45,6 +48,7 @@ contract Hackathoncontract {
     }
 
     function claim_bounty_dev(bytes32 _hash) public returns (bool success) {
+        assert(DEV_MODE);
         bounty_map[_hash] = safe_subtract(bounty_map[_hash], payout_map[_hash]);
         msg.sender.transfer(payout_map[_hash]);
         return true;
@@ -55,6 +59,13 @@ contract Hackathoncontract {
         assert(escrowed_bounties[_recipient] >= _amt);
         escrowed_bounties[_recipient] = safe_subtract(escrowed_bounties[_recipient], _amt);
         _recipient.transfer(_amt);
+        return true;
+    }
+
+    function set_dev_mode(bool _mode) public returns (bool success) {
+        require(msg.sender == owner);
+        if(DEV_MODE) DEV_MODE = false;
+        else {DEV_MODE = true;}
         return true;
     }
 }

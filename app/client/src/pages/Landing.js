5c2cc5bb7c47cdf68e29c5bb171eea9af7528206
${Michael} <michael.dannunzio3@gmail.com>
2018-10-15 18:51:29 -0500

    fuck the landing page

--- app/client/src/pages/Landing.js
+++ app/client/src/pages/Landing.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 import '../css/Landing.css';
-import params from '../config/Landing.config';
+import params from '../config/Landing.config'; 
 
 class Landing extends React.Component {
 	render() {
 		return(
 			<div className='Landing'>
-				<Particles
-					params={params}
-				/>
+				{/* <div className='welcome'>Welcome to 3IE.IO</div> */}
+				<Particles params={params} className='Particles' />
+				{/* <div className='info'>
+					Where Hashgraphers (FARM)
+						<ul>
+							<li>Familiarize with the tech</li>
+							<li>Align with the community</li>
+							<li>Rely on the information/data</li>
+							<li>Mature their invention, skillset, knowledge</li>
+						</ul>
+				</div> */}
 			</div>
-		
 		);
-	}	
+	}
 }
 
 export default Landing;

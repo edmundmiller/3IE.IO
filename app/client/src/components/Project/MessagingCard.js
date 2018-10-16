dfde2653a6ba4ab989f7f79369216dc2f61bd05d
${Michael} <michael.dannunzio3@gmail.com>
2018-10-16 11:44:34 -0500

    same old shit

--- app/client/src/components/Project/MessagingCard.js
+++ app/client/src/components/Project/MessagingCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ExampleProjectLogo from '../../images/ncUG3A1O_400x400.jpg';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
-import TeamContainer from './TeamContainer';
+import TeamContainer from './teamContainer';
 import styles from '../../css/ProjectDashboard.css';
 
 const MessagingCard = ({ }) => {
--- app/client/src/components/Project/ProjectHeader.js
+++ app/client/src/components/Project/ProjectHeader.js
@@ -3,7 +3,7 @@ import ExampleProjectLogo from '../../images/ncUG3A1O_400x400.jpg';
 import Ourlogo from '../../resources/3ie.io.svg';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
-import TeamContainer from './TeamContainer';
+import TeamContainer from './teamContainer';
 import styles from '../../css/ProjectDashboard.css';
 
 const ProjectHeader = ({ }) => {

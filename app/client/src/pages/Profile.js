1a124c05eae4b47c25a9f770f980ddb86115d230
${Michael} <michael.dannunzio3@gmail.com>
2018-10-16 00:53:38 -0500

    profile stuff

--- app/client/src/pages/Profile.js
+++ app/client/src/pages/Profile.js
@@ -1,20 +1,31 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
+import Avatar from '../components/profile/Avatar';
+// import Avatar from '@material-ui/core/Avatar';
+import Info from '../components/profile/Info';
 
 class Profile extends Component {
   constructor (props) {
-    super(props)
-    this.state ={
+    super(props);
+    this.state = {
       projects: null,
-    }
+      balance: null
+    };
   }
 
   render() {
-    return(
+    return (
       <div>
-        <h1 style={{color: 'white'}}>Hello, this is profiles page</h1>
+        {/* <Avatar
+          alt="Leemon Baird"
+          src="../images/Leemon-752x506.jpg"
+          className='avatar-profile'
+        /> */}
+        <Avatar />
+        <Info />
       </div>
-    )
+    );
   }
 }
 
-export default Profile
+export default Profile;

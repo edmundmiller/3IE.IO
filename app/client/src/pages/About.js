1f99d834ea1a73ebc8e5600f5aa7b4a068007a91
Luis Osta <luisosta1999@gmail.com>
2018-10-15 19:41:18 -0500

    finalized landing page and navbar

---
+++ app/client/src/pages/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class LogIn extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Hello this is the about page</h1>
+      </div>
+    )
+  }
+}
+
+export default LogIn
